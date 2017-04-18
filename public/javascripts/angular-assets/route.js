9fde0764702b20b599e8ad5c1767abae72d02ea2
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-04-18 17:01:46 +0700

    init route

--- public/javascripts/angular-assets/route.js
+++ public/javascripts/angular-assets/route.js
@@ -12,8 +12,45 @@ app.config([
         $stateProvider
             .state('subject-index', {
                 url: '/subject-index',
-                templateUrl: '/javascripts/angular-assets/partialviews/indexsubject.html'
+                templateUrl: '/javascripts/angular-assets/partialviews/subjects/index.html'
+            });       
+        $stateProvider
+            .state('subject-create', {
+                url: '/subject-create',
+                templateUrl: '/javascripts/angular-assets/partialviews/subjects/create.html'
+            });        
+        $stateProvider
+            .state('subject-detail', {
+                url: '/subject-detail',
+                templateUrl: '/javascripts/angular-assets/partialviews/subjects/detail.html'
             });
+        $stateProvider
+            .state('subject-edit', {
+                url: '/subject-edit',
+                templateUrl: '/javascripts/angular-assets/partialviews/subjects/edit.html'
+            });
+
+        $stateProvider
+            .state('question-index', {
+                url: '/question-index',
+                templateUrl: '/javascripts/angular-assets/partialviews/questions/index.html'
+            });       
+        $stateProvider
+            .state('question-create', {
+                url: '/question-create',
+                templateUrl: '/javascripts/angular-assets/partialviews/questions/create.html'
+            });        
+        $stateProvider
+            .state('question-detail', {
+                url: '/question-detail',
+                templateUrl: '/javascripts/angular-assets/partialviews/questions/detail.html'
+            });
+        $stateProvider
+            .state('question-edit', {
+                url: '/question-edit',
+                templateUrl: '/javascripts/angular-assets/partialviews/questions/edit.html'
+            });
+
 
     }
 ]);
