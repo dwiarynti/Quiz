29e6e8627c1d3830d720a53438c7a02f89b0e2ec
Rjuzariah <riajuzariah@gmail.com>
2017-05-04 09:21:49 +0700

    update app.js

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ var choices = require('./routes/choices');
 var submitquiz = require('./routes/submitquiz');
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+// app.listen(server_port, server_ip_address, function () {
+//   console.log( "Listening on " + server_ip_address + ", port " + server_port )
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
