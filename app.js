82788bbae7a4b93528a479a7012953057f6ee33e
Rjuzariah <riajuzariah@gmail.com>
2017-05-04 09:18:29 +0700

    update app.js

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ passport.deserializeUser(Account.deserializeUser());
 //   }
 // ));
 //conncet to database
+// mongoose.connect('mongodb://192.168.1.99:27017/Quiz_db');
 mongoose.connect('mongodb://userGL3:5HHV7bkAwUgg18Qe@mongodb/Quiz_db');
 
 // catch 404 and forward to error handler
