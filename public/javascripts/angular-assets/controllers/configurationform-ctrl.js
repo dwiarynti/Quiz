390313af4937276300e6e8f2a15da5584d549b03
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-05-15 14:47:35 +0700

    update config form

--- public/javascripts/angular-assets/controllers/configurationform-ctrl.js
+++ public/javascripts/angular-assets/controllers/configurationform-ctrl.js
@@ -11,6 +11,7 @@ $scope.btnAddClick = function()
     $scope.showbtn = true;
     $scope.profilefield.push({
         "_id":0,
+        "editmode" : false,
         "key" : "",
         "type": "",
         "templateOptions": {
@@ -43,7 +44,7 @@ $scope.AddClick = function(obj)
     //console.log(configurationformresource.data);
     configurationformresource.$add().then(function(item)
     {
-        console.log("i",item);
+        //console.log("i",item);
         $scope.init();
         
 
@@ -55,9 +56,39 @@ $scope.init = function()
 {
     configurationformresource.$init().then(function(data)
     {
-        $scope.profilefield = data.Obj;
+       
+        angular.forEach(data.Obj,function(item) {
+                 item.editmode = false;                 
+                $scope.profilefield.push(item);
+            });
         console.log($scope.profilefield);
     })
 }
 $scope.init();
+
+$scope.btnUpdateClick = function(obj)
+{
+    obj.editmode = true;
+}
+
+$scope.UpdateClick = function(obj)
+{
+     console.log(obj);
+     configurationformresource._id =obj._id;
+     configurationformresource.type = obj.type;
+     configurationformresource.key = obj.key;
+     configurationformresource.templateOptions = obj.templateOptions;
+    
+     configurationformresource.$update().then(function(data)
+     {
+        
+         $scope.profilefield = [];
+         $scope.init();
+     })
+}
+
+$scope.turnoffeditmode = function(obj){
+    obj.editmode = false;    
+}
+
 })
--- routes/configurationForm.js
+++ routes/configurationForm.js
@@ -25,7 +25,7 @@ router.post('/configurationform/create', function(req,res)
 		var obj = req.body.data[i];
 		ConfigurationFromcollection.insert(obj, function(err,data)
 		{
-		if(data.length == req.body.data.length)
+		if(i == req.body.data.length)
 		{
 			res.json({success:true})
 		}
@@ -33,14 +33,34 @@ router.post('/configurationform/create', function(req,res)
 	}
 });
 
+
 router.get('/configurationform/init', function(req,res)
 {
 		ConfigurationFromcollection.find({"formname":"Profile"}, function(err, obj){
 		if(err) {res.json(500, err);}
 		else
         { 
-          res.json({'Obj': obj,'success': true});
+          res.json({'Obj': obj, success: true});
         }
 	});
-})
+});
+router.post('/configurationform/Update/:_id', function(req,res)
+{
+	var id = ObjectId(req.params._id);
+	ConfigurationFromcollection.update({'_id':id},{
+	$set :{
+		'formname': req.body.data.formname,
+		'key': req.body.data.key,
+		'type': req.body.data.type,
+		'templateOptions.type' : req.body.data.type,
+		'templateOptions.label' : req.body.data.label,
+		'templateOptions.placeholder' : req.body.data.placeholder,
+
+	} ,function(err)
+	{
+	  if(err) res.json(500,err)
+      else res.json({success : true});
+	}
+	});
+});
 module.exports = router;
